3ca9b65b323712ea9617921c2007cc9c4424e8d3
prajnan62-ui <prajnan62@gmail.com>
2026-02-24 21:09:56 -0700

    file modified

--- src/component/Component3.jsx
+++ src/component/Component3.jsx
@@ -11,9 +11,9 @@ function Component3(){
         <div className="container">
             <div className="top">
                 <div className="left">
-                    <div className="content"><b>GET IN TOUCH </b>
+                    <div className="content"><b>Get In Touch </b>
                     </div>
-                    <div className="content1">we'd love to hear from you.Reach out with questions.</div>
+                    <div className="content1">we'd love to hear from you.Reach out with questions,feedback,or to book a service</div>
                     <div className="content2"><FaPhone />  9834556681 </div>
                     <div className="content2"><IoMail />  Vedhashree Professional Beauty Parlour and Makeup Studio.com </div>
                     <div className="content1"><IoLocation style color='rgb(224, 79, 128)' />   Bargi,Kumta</div>
@@ -23,11 +23,11 @@ function Component3(){
 
                 </div>
                 <div className="right">
-                    {/* <div className="right1"> */}
+                     <div className="right1">
                         <div className="r1"><b><GoClockFill />   Parlour Timings</b></div>
-                        <div className="r2">Opening:10:00AM</div>
-                        <div className="r2">Closeing:8:00AM</div>
-                    {/* </div> */}
+                        <div className="r2"><b>Opening:</b>10:00AM</div>
+                        <div className="r2"><b>Closeing:</b>8:00AM</div>
+                    </div> 
                 </div>
              </div>
              <div className="Bottom"></div>
